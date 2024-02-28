20ca148a22c88489c55f4e1722068df09b34082b
Seyed Amir Ahmad Safvi-Naini <104253704+Sdamirsa@users.noreply.github.com>
2024-02-28 19:34:40 +0330

    Update zero shot.py

--- zero shot.py
+++ zero shot.py
@@ -1,3 +1,4 @@
+# loading libraries
 import pandas as pd
 import time
 from transformers import AutoTokenizer
@@ -5,29 +6,22 @@ from datasets import load_dataset
 from transformers import pipeline
 from huggingface_hub import notebook_login
 
+# loading the data and the classifier model and tokenizer, which is bart-large-mnli
 notebook_login()
-
-
 classifier = pipeline("zero-shot-classification",
                       model="facebook/bart-large-mnli")
 
-
 dataset = load_dataset("Moreza009/internal_LLM_num")
+train_ds = dataset["test"] #!!! change to test_ds
 
+t = AutoTokenizer.from_pretrained("facebook/bart-large-mnli") # please rename t to tokenizer
 
-train_ds = dataset["test"]
-
-
-t = AutoTokenizer.from_pretrained("facebook/bart-large-mnli")
-
-
+# Getting the output on the test set for zero-shot classification on internal validation (test set)
 start_time = time.time()
-
 list_results = []
 count = 0
-
 for i in train_ds["patient medical hidtory"]:
-    sequence_to_classify = t(i)
+    sequence_to_classify = t(i) #!!! explain or remove the t function
     candidate_labels = [1, 0]
     classing = classifier(sequence_to_classify, candidate_labels)
     scores = classing["scores"]
@@ -37,30 +31,26 @@ for i in train_ds["patient medical hidtory"]:
     list_results.append(result)
     count += 1
     print(count)
-
+  
+# printing the result and time of exec
 print(list_results)
-
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(f"Time elapsed: {elapsed_time} seconds")
 
-
+# Saving the outputs for internal validation
 df = pd.DataFrame(
     {"y_pred": list_results, "y_true": train_ds["Inhospital Mortality"]})
-
-
 df.to_excel("zero_shot_internal.xlsx", index=False)
 
 
+# Getting the output on the test set for zero-shot classification on external validation
 dataset = load_dataset("Moreza009/External_validation_num")
-train_ds = dataset["train"]
-
+train_ds = dataset["train"] #!!! correct the naming plz
 
 start_time = time.time()
-
 list_results = []
 count = 0
-
 for i in train_ds["patient medical hidtory"]:
     sequence_to_classify = t(i)
     candidate_labels = [1, 0]
@@ -73,15 +63,13 @@ for i in train_ds["patient medical hidtory"]:
     count += 1
     print(count)
 
+# printing the result and exec time
 print(list_results)
-
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(f"Time elapsed: {elapsed_time} seconds")
 
-
+# Saving the outputs for external validation
 df = pd.DataFrame(
     {"y_pred": list_results, "y_true": train_ds["Inhospital Mortality"]})
-
-
 df.to_excel("zero_shot_external.xlsx", index=False)
