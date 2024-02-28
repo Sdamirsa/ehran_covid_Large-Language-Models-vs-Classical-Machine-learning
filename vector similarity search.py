7537b935b3510ee294cc84eb6bf6ec3882af4da6
Seyed Amir Ahmad Safvi-Naini <104253704+Sdamirsa@users.noreply.github.com>
2024-02-28 19:44:26 +0330

    Update vector similarity search.py

--- vector similarity search.py
+++ vector similarity search.py
@@ -1,40 +1,35 @@
+# loading the libraries
 from langchain.vectorstores import FAISS
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from datasets import load_dataset
 import pandas as pd
 from datasets import load_dataset
 
-
+# loading and preparing the data
 data = load_dataset("Moreza009/Internal_validation")
 
-
 def merge_columns(example):
     example["prediction"] = example["patient medical hidtory"] + \
         " ----->: " + str(example["Inhospital Mortality"])
     return example
 
+data['train'] = data['train'].map(merge_columns) #!!! plz check if this works # please print this data and send the picture to me plz
 
-data['train'] = data['train'].map(merge_columns)
+documents = data['train']["prediction"] #!!! rename the documents
 
-
-documents = data['train']["prediction"]
-
+# loading the embedding model
 embeddings = HuggingFaceEmbeddings()
-
 knowledge_base = FAISS.from_texts(documents, embeddings)
 
-
 def merge_columns(example):
     example["prediction"] = example["patient medical hidtory"] + \
         " ----->: " + str(example["Inhospital Mortality"])
     return example
 
-
-data['test'] = data['test'].map(merge_columns)
-
+# finding the test accuracy using the retriver
+data['test'] = data['test'].map(merge_columns) #!!! this is incorrect
 
 data['test']["prediction"]
-
 
 y_pred = []
 for i in data['test']["prediction"]:
